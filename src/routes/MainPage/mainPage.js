317aa9a8da186113242df81d818fb0d4a0942149
millifail <hwangbird99@gmail.com>
2023-07-03 19:08:56 +0900

    [FE-02] header 구현

--- src/routes/MainPage/mainPage.js
+++ src/routes/MainPage/mainPage.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Header from '../../components/Header';
+import './mainPage.css';
 
 function mainPage() {
-  return <div>mainPage</div>;
+  return (
+    <>
+      <div className='viewport'>
+        <div className='contents'>
+          <Header />
+          <div className='main-banner'>
+            <h1>
+              Let's
+              <br />
+              AhPuh!
+            </h1>
+            <button>서핑 시작하기</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default mainPage;
