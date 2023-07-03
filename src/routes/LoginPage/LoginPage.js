c2e82d8a005172f17114849207d63221cb207c8b
millifail <hwangbird99@gmail.com>
2023-07-04 02:15:45 +0900

    css 오류 수정

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import Header from '../../components/Header';
-import './LoginPage.css';
+import styles from './LoginPage.module.css';
 import picture from '../../image/main_picture.jpg';
 import Footer from './../../components/Footer';
 
 function LoginPage() {
   return (
     <>
-      <div className='viewport'>
-        <div className='contents'>
+      <div className={styles.viewport}>
+        <div className={styles.contents}>
           <Header />
-            <img className='img' src={picture} alt='Main Picture' />
-          <div className='login-container'>
-            <h1 className='login-title'>로그인</h1>
-            <div className='input-container'>
+          <img className={styles.img} src={picture} alt='Main Picture' />
+          <div className={styles.login_container}>
+            <div className={styles.input_container}>
               <input
-                className='input-field'
+                className={styles.input_field}
                 type='text'
                 placeholder='ID'
               />
             </div>
-            <div className='input-container'>
+            <div className={styles.input_container}>
               <input
-                className='input-field'
+                className={styles.input_field}
                 type='password'
                 placeholder='PASSWORD'
               />
             </div>
-            <button className='login-button'>로그인</button>
-            <button className='signup-button'>회원가입</button>
+            <button className={styles.login_button}>로그인</button>
+            <button className={styles.signup_button}>회원가입</button>
           </div>
         </div>
         <Footer />
