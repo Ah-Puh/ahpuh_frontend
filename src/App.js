235565b14ab30e9ed030d1870eda834b8d835742
millifail <hwangbird99@gmail.com>
2023-07-03 16:54:19 +0900

    folder 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './routes/LandingPage';
-import Days from './routes/Days';
+import DayPage from './routes/DayPage/DayPage';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path='/' element={LandingPage()} />
-        <Route exact path='/days' element={Days()} />
+        <Route exact path='/days' element={DayPage()} />
       </Routes>
     </div>
   );
---
+++ src/routes/DayPage/DayPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DayPage() {
+  return <div>DayPage</div>;
+}
+
+export default DayPage;
