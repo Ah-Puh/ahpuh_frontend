3874f77bd34be4a1516dbb86f01a3f0add5afb94
millifail <hwangbird99@gmail.com>
2023-07-03 17:04:48 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import LandingPage from './routes/LandingPage';
 import DayPage from './routes/DayPage/DayPage';
+import mainPage from './routes/MainPage/mainPage';
+import LocationPage from './routes/LocationPage/LocationPage';
+import TutorPage from './routes/TutorPage/TutorPage';
+import TutorDetailPage from './routes/TutorDetailPage/TutorDetailPage';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path='/' element={LandingPage()} />
+        <Route exact path='/' element={mainPage()} />
+        <Route exact path='/location' element={LocationPage()} />
         <Route exact path='/days' element={DayPage()} />
+        <Route exact path='/tutor' element={TutorPage()} />
+        <Route exact path='/tutordetail/:tutorId' element={TutorDetailPage()} />
       </Routes>
     </div>
   );
---
+++ src/routes/TutorDetailPage/TutorDetailPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TutorDetailPage() {
+  return <div>TutorDetailPage</div>;
+}
+
+export default TutorDetailPage;
