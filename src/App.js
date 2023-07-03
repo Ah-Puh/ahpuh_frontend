5756d7c1103db4428a3079ede26263048897601a
Dawon00 <dawon000203@gmail.com>
2023-07-03 22:59:10 +0900

    [FE-11] 날짜 선택 페이지 url 에 location url params 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import DayPage from './routes/DayPage/DayPage';
-import mainPage from './routes/MainPage/mainPage';
-import LocationPage from './routes/LocationPage/LocationPage';
-import TutorPage from './routes/TutorPage/TutorPage';
-import TutorDetailPage from './routes/TutorDetailPage/TutorDetailPage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import DayPage from "./routes/DayPage/DayPage";
+import mainPage from "./routes/MainPage/mainPage";
+import LocationPage from "./routes/LocationPage/LocationPage";
+import TutorPage from "./routes/TutorPage/TutorPage";
+import TutorDetailPage from "./routes/TutorDetailPage/TutorDetailPage";
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route exact path='/' element={mainPage()} />
-        <Route exact path='/location' element={LocationPage()} />
-        <Route exact path='/days' element={DayPage()} />
-        <Route exact path='/tutor' element={TutorPage()} />
-        <Route exact path='/tutordetail/:tutorId' element={TutorDetailPage()} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route exact path="/" element={mainPage()} />
+                <Route exact path="/location" element={LocationPage()} />
+                <Route exact path="/days/:location" element={DayPage()} />
+                <Route exact path="/tutor" element={TutorPage()} />
+                <Route
+                    exact
+                    path="/tutordetail/:tutorId"
+                    element={TutorDetailPage()}
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
